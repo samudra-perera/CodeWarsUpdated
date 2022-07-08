fe8d8247ee5c479ddc0dcfc14e345e0e6557e81c
Samudra Perera <samudrapup@gmail.com>
2022-07-08 14:17:23 -0400

    july 8 codewars

--- julyWars.js
+++ julyWars.js
@@ -453,3 +453,73 @@ function choreAssignment(chores) {
 }
 
 choreAssignment([5, 2, 1, 6, 4, 4])
+
+/* Function receive a two-dimensional square array of random integers. On the main diagonal, all the negative integers must be changed to 0, while the others must be changed to 1 (Note: 0 is considered non-negative, here).
+
+(You can mutate the input if you want, but it is a better practice to not mutate the input)
+
+Example:
+
+Input array
+
+[
+  [-1,  4, -5, -9,  3 ],
+  [ 6, -4, -7,  4, -5 ],
+  [ 3,  5,  0, -9, -1 ],
+  [ 1,  5, -7, -8, -9 ],
+  [-3,  2,  1, -5,  6 ]
+]
+Output array
+
+[
+  [ 0,  4, -5, -9,  3 ],
+  [ 6,  0, -7,  4, -5 ],
+  [ 3,  5,  1, -9, -1 ],
+  [ 1,  5, -7,  0, -9 ],
+  [-3,  2,  1, -5,  1 ]
+]
+*/
+
+function matrix(array) {
+    let len = array.length;
+    for(let i = 0; i < len; i++) {
+      if(array[i][i] >= 0) {
+        array[i][i] = 1
+      } else {
+        array[i][i] = 0
+      }
+    }
+    return array
+}
+
+/* Task
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Notes
+Array/list size is at least 2.
+
+Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+Input >> Output Examples
+adjacentElementsProduct([1, 2, 3]); ==> return 6
+Explanation:
+The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+Explanation:
+Max product obtained from multiplying 5 * 10 = 50 .
+
+adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])  ==>  return -14
+Explanation:
+The maximum product obtained from multiplying -2 * 7 = -14, and they're adjacent numbers in the array.
+*/
+
+function adjacentElementsProduct(array) {
+  newArr = [];
+  for(let i = 0; i < array.length - 1; i++) {
+    newArr[i] = array[i]*array[i+1];
+  }
+  return newArr.sort((a,b) => a-b)[newArr.length - 1]
+}
+
+adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])
+adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48])
