c9ff44cb40f705086a428139bf6b3b88807a1cab
Samudra Perera <samudrapup@gmail.com>
2022-07-14 12:31:23 -0400

    July 14 codewars #1

--- julyWars.js
+++ julyWars.js
@@ -581,3 +581,178 @@ function friend(friends){
 
 // Done for Today
 
+
+/// JS Practice OOP
+
+// TODO: define the Dog class here
+
+class Dog {
+  constructor(name, species, size) {
+    this.name = name;
+    this.species = species;
+    this.size = size;
+  }
+
+  bark() {
+    if(this.size < 50) {
+      return 'Grrr Grrr'
+    } else {
+      return 'Woof Woof'
+    }
+  }
+}
+
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
+
+
+// OOOP RPG Example
+class Character {
+  constructor(name, health, strength, gold, key) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    this.gold = 10;
+    this.key = 1;
+    this.xp = 0; // XP is always zero for new characters
+  }
+  // Attack a target
+  attack(target) {
+    if (this.health > 0) {
+      const damage = this.strength;
+      console.log(
+        `${this.name} attacks ${target.name} and causes ${damage} damage points`
+      );
+      target.health -= damage;
+      if (target.health > 0) {
+        console.log(`${target.name} has ${target.health} health points left`);
+      } else {
+        target.health = 0;
+        const bonusXP = 10;
+        console.log(`${this.name} eliminated ${target.name} and wins ${bonusXP} experience points, ${target.gold} gold and ${target.key} key(s)`);
+        this.xp += bonusXP;
+        this.gold = this.gold + target.gold;
+        this.key = this.key + target.key;
+        target.gold = 0;
+        target.key = 0;
+      }
+    } else {
+      console.log(`${this.name} can't attack (they've been eliminated)`);
+    }
+  }
+  // Return the character description
+  describe() {
+    return `${this.name} has ${this.health} health points, ${this
+      .strength} as strength and ${this.xp} XP points, ${this.gold} gold and ${this.key} key(s)`;
+  }
+}
+
+const aurora = new Character("Aurora", 150, 25);
+const glacius = new Character("Glacius", 130, 30);
+
+console.log("Welcome to the adventure! Here are our heroes:");
+console.log(aurora.describe());
+console.log(glacius.describe());
+
+const monster = new Character("Spike", 40, 20);
+console.log("A wild monster has appeared: it's named " + monster.name);
+
+monster.attack(aurora);
+monster.attack(glacius);
+aurora.attack(monster);
+glacius.attack(monster);
+
+console.log(aurora.describe());
+console.log(glacius.describe());
+
+
+///OOP Last Example 
+
+class Account {
+  constructor(name, balance) {
+    this.name = name;
+    this.balance = 0; 
+  }
+
+  credit(value) {
+    this.balance = this.balance + value;
+  }
+
+  describe() {
+    console.log(`owner: ${this.name}, balance: ${this.balance}`)
+  }
+}
+
+const sean = new Account('Sean', 0)
+const brad = new Account('Brad', 0)
+const george = new Account('George', 0)
+sean.credit(1000)
+brad.credit(1000)
+george.credit(1000)
+sean.describe()
+brad.describe()
+george.describe()
+
+/// July 14 Codewars 
+/* Task
+Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+
+Notes
+Array/list size is at least 2 .
+
+Array/list's numbers Will be only Positives
+
+Repetition of numbers in the array/list could occur.
+
+Input >> Output Examples
+productArray ({12,20}) ==>  return {20,12}
+Explanation:
+The first element in prod [] array 12 is the product of all array's elements except the first element
+
+The second element 20 is the product of all array's elements except the second element .
+
+productArray ({1,5,2}) ==> return {10,2,5}
+Explanation:
+The first element 10 is the product of all array's elements except the first element 1
+
+The second element 2 is the product of all array's elements except the second element 5
+
+The Third element 5 is the product of all array's elements except the Third element 2.
+
+productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
+Explanation:
+The first element 180 is the product of all array's elements except the first element 10
+
+The second element 600 is the product of all array's elements except the second element 3
+
+The Third element 360 is the product of all array's elements except the third element 5
+
+The Fourth element 300 is the product of all array's elements except the fourth element 6
+
+Finally ,The Fifth element 900 is the product of all array's elements except the fifth element 2
+*/
+
+function productArray(numbers){
+  let productArray = [];
+  
+  for(let i = 0; i < numbers.length; i++) {
+    let method = numbers.map(elem => elem)
+    method.splice(i,1)
+    productArray[i] = method.reduce((acc, cur) => acc * cur, 1)
+  }
+  return productArray
+}
+productArray([3,27,4,2])
+
+// Alternative Solution using divison
+function productArray(numbers){
+  let product = numbers.reduce((acc, cur) => acc * cur, 1);
+  return numbers.map(elem => product/elem)
+}
+productArray([3,27,4,2])
+
