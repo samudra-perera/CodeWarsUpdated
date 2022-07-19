7a23e8bdd2a59c570c51aa434012b9a9ceef5e91
Samudra Perera <samudrapup@gmail.com>
2022-07-19 11:56:11 -0400

    Return the single odd or even digit in an array 6Kyu

--- julyWars.js
+++ julyWars.js
@@ -1296,3 +1296,30 @@ function duplicateCount(text){
   //Object.values returns only the count into an array then it is filter for only those repeating (count greater than 1)
   return Object.values(objSet).filter(elem => elem > 1).length
 }
+
+/* 
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+*/
+
+function findOutlier(integers){
+  let even = [];
+  let odd = [];
+
+  for(let i = 0; i < integers.length; i++) {
+    if(integers[i] % 2 == 0) {
+      even.push(integers[i])
+    } else {
+      odd.push(integers[i])
+    }
+  }
+
+  return even.length == 1 ? even[0] : odd[0];
+
+}
