e89c381b636ef447d86ef6f1357669b5cb7bd074
Samudra Perera <samudrapup@gmail.com>
2022-07-18 23:17:30 -0400

    6 Kyu twin primes up to N

--- julyWars.js
+++ julyWars.js
@@ -1147,3 +1147,50 @@ function pigIt(str){
 }
 
 pigIt('Pig latin is cool')
+
+/* A twin prime is a prime number that is either 2 less or 2 more than another prime number—for example, either member of the twin prime pair (41, 43). In other words, a twin prime is a prime that has a prime gap of two. Sometimes the term twin prime is used for a pair of twin primes; an alternative name for this is prime twin or prime pair. (from wiki https://en.wikipedia.org/wiki/Twin_prime)
+
+Your mission, should you choose to accept it, is to write a function that counts the number of sets of twin primes from 1 to n.
+
+If n is wrapped by twin primes (n-1 == prime && n+1 == prime) then that should also count even though n+1 is outside the range.
+
+Ex n = 10
+
+Twin Primes are (3,5) (5,7) so your function should return 2!
+*/
+
+function twinPrime(n) {
+  let prime = [];
+  let sieve = [];
+  for(let i = 2; i <= n; i++) {
+    if(!sieve[i]) {
+      prime.push(i)
+    }
+    for(j = i; j <= n; j += i) {
+      sieve[j] = true;
+      console.log(j)
+    }
+  }
+  return prime
+}
+
+twinPrime(100)
+
+function twinPrime(n) {
+  let array = [...Array(n+1).keys()];
+  let upperLimit = Math.sqrt(n);
+
+
+  // returns an array of primes = numbers and non=primes = false
+  for(let i = 2; i <= upperLimit; i++) {
+    if(array[i]) {
+      for(let j = i * i; j < n; j += i) {
+        console.log(j)
+        array[j] = false;
+      }
+    }
+  }
+  return array
+}
+
+twinPrime(100)
