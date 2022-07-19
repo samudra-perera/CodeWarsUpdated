fb4aafbe5f1b2664e975dbb272fddc58cd4f3a09
Samudra Perera <samudrapup@gmail.com>
2022-07-19 00:39:00 -0400

    6 kyu kata removing elements from array a using array b

--- julyWars.js
+++ julyWars.js
@@ -1243,3 +1243,25 @@ function createPhoneNumber(n) {
 }
 
 createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
+/*
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+*/
+
+function arrayDiff(a, b) {
+  return a.filter(elem => !b.includes(elem))
+}
+
+/* 
+b.includes(elem) returns a true of false value and the bang operator inverses to not include those values
+*/
+
+  arrayDiff([1,2,3], [1,2])
+
