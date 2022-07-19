eebef3e302ccb38c145cf0b40450c12db7f99512
Samudra Perera <samudrapup@gmail.com>
2022-07-19 00:15:00 -0400

    6kyu phone number string replace

--- julyWars.js
+++ julyWars.js
@@ -1207,3 +1207,39 @@ function twinPrime(n) {
   }
   return twin.length
 }
+
+/*
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+*/
+
+function createPhoneNumber(n){
+  return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${9}`
+
+}
+
+function createPhoneNumber(n) {
+  let newArr = n.map(elem => elem.toString())
+  newArr.splice(0,0, '(')
+  newArr.splice(4,0, ')')
+  newArr.splice(5,0,' ')
+  newArr.splice(9,0, '-')
+  return newArr.join('')
+}
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
+// Better solution
+function createPhoneNumber(n) {
+  let format = '(xxx) xxx-xxxx'
+  for(let i = 0; i < n.length; i++) {
+    format = format.replace('x', n[i])
+  }
+  return format
+}
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
