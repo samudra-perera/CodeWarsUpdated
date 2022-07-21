8e59ad5c075607a8c5c8b20f41d548f3fb0e9ca3
Samudra Perera <samudrapup@gmail.com>
2022-07-20 22:39:54 -0400

    6kyu direction array

--- julyWars.js
+++ julyWars.js
@@ -1443,17 +1443,52 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 
 
 function alphabetPosition(text) {
+  //Object dictionary with corresponding values
   let alpha = {
     'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
     'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
   }
 
-  
+  //Turn string into lowercase then removing spaces and turning into array once more then mapping char to obj and filteing undefined elements out
   return text.toLowerCase().split(' ').join('').split('').map(elem => {
     if(alpha[elem]) {
       return alpha[elem]
     }
-  }).filter(elem => typeof(elem) == 'number').join('')
+  }).filter(elem => typeof(elem) == 'number').join(' ')
 }
 
 alphabetPosition("The sunset sets at twelve o' clock.")
+
+/*
+You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+*/
+
+function isValidWalk(walk) {
+  // assigning values to directions
+  let direction = {
+    'n': 1,
+    's': -1,
+    'w': 1,
+    'e': -1
+  }
+
+  //cumulation of the position of vertical and horizontal
+  let [vert, horz] = [0,0]
+
+  //For loop iterating through the array and matching the direction with the obj value and adding to total pos array
+  for(let i = 0; i < walk.length; i++) {
+    if(walk[i] == 'n' || walk[i] == 's') {
+      vert+=direction[walk[i]]
+    } else if (walk[i] == 'w' || walk[i] == 'e') {
+      horz+=direction[walk[i]]
+    }
+  }
+  
+  return (vert == 0 && horz == 0 && walk.length == 10)
+}
+
+isValidWalk(['n','s','n','s','n','s','n','s','n','s'])
+
