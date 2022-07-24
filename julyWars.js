12ce7e436e234d190c130e2b2a98e4aded1f77e7
Samudra Perera <samudrapup@gmail.com>
2022-07-24 18:54:14 -0400

    5 kyu anagram comparison

--- julyWars.js
+++ julyWars.js
@@ -1741,3 +1741,31 @@ function narcissistic(value) {
   return value == value.toString().split('').reduce((acc,cur) => acc + Number(cur)**len, 0)
 }
 
+/*
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+*/
+
+function anagrams(word, words) {
+  // Create a sorted word according to alpha
+  let newWord = word.split('').sort().join('')
+
+  //filters words array using newWord as requirement
+  return words.filter(elem => elem.split('').sort().join('') == newWord)
+}
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])
