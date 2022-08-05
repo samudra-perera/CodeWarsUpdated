480698c959147521a765728b09e1d8e2a80de535
Samudra Perera <samudrapup@gmail.com>
2022-08-05 15:48:46 -0400

    6kyu create a set of letters

--- Aug 05 2022/main.js
+++ Aug 05 2022/main.js
@@ -47,5 +47,33 @@ function longestConsec(strarr, k) {
     return concat[lens.indexOf(Math.max(...lens))]
 }
 
-longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2)
 
+/* 
+
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+
+*/
+
+function count (string) {  
+    let obj = {};
+    for(let i = 0; i < string.length; i++) {
+        let char = string.charAt(i)
+        if(obj[char]) {
+            obj[char]++
+        } else {
+            obj[char] = 1
+        }
+    }
+     return obj;
+  }
+  
+  // Done with forEach()
+  function count (string) {  
+    let count = {};
+    string.split('').forEach(function(s) {
+       count[s] ? count[s]++ : count[s] = 1;
+    });
+    return count;
+  }
