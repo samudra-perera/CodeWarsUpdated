f039f09a48d5d08854431f79994d1cf067c0546d
Samudra Perera <samudrapup@gmail.com>
2022-08-05 22:26:53 -0400

    Building a cube tower 6kyu

--- Aug 05 2022/main.js
+++ Aug 05 2022/main.js
@@ -112,3 +112,30 @@ function titleCase(title, minorWords) {
 }
 
 titleCase('the clash of KINGS', 'a an the of')
+
+/* Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+
+Examples:
+findNb(1071225) --> 45
+
+findNb(91716553919377) --> -1
+*/
+
+function findNb(m) {
+    let count = 1;
+    let remainder = m
+    while(remainder > 0) {
+        remainder -= (count**3)
+        if(remainder == 0) {
+            break
+        }
+        count++
+    }
+    return (remainder == 0) ? count : -1
+}
+
+findNb(1071225)
