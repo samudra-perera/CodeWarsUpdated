c557ea003f0ff0c4bc5fd5b7193fbe68ca4adbf3
Samudra Perera <samudrapup@gmail.com>
2022-08-06 02:57:52 -0400

    6 Kyu Expanded Form

--- Aug 06 2022/main.js
+++ Aug 06 2022/main.js
@@ -9,12 +9,49 @@ For example (Input --> Output):
 
 function persistence(num) {
     //Counter to keep track of the number of steps
-    let counter = 0
+    let counter = 0;
     while(num.toString().length > 1) {
         //Turn num into an array then reduce then increment counter 
-        num = num.toString().split('').reduce((acc, curr) => Number(acc) * Number(curr), 1)
-        counter++
+        num = num.toString().split('').reduce((acc, curr) => Number(acc) * Number(curr), 1);
+        counter++;
     }
-    return counter
+    return counter;
  }
 
+/* Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
+*/
+
+function expandedForm(num) {
+    num = num.toString().split('');
+    //to store the strings of digits in the addition sequence
+    let store = [];
+    for(let i = 0; i < num.length; i++) {
+        if(num[i] == 0) {
+            //if num == 0 don't do anything continue
+            continue;
+        } else {
+            //Any non zero digit push to store and add the number of zeros needed 
+            store.push(num[i] + '0'.repeat(num.length - 1 - i));
+        }
+    }
+    
+    //For loop to create the addition string
+    let str = '';
+    for(let j = 0; j < store.length; j++) {
+        //if statement to handle the last element 
+        if(j == store.length - 1) {
+            str+= `${store[j]}`
+            break
+        }
+        str+= `${store[j]} + `
+    }
+    return str
+  }
+
+  expandedForm(70304)
