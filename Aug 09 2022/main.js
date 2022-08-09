bd0bf07cfe7320467f99c4c2aca4aed9c9b665bc
Samudra Perera <samudrapup@gmail.com>
2022-08-09 13:39:14 -0400

    5 Kyu Hashtag Generator

--- Aug 09 2022/main.js
+++ Aug 09 2022/main.js
@@ -69,3 +69,23 @@ var getNum = function(n) {
     })
     return count
 };
+
+/* The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+*/
+function generateHashtag (str) {
+    let hashtag = str.split(' ').map(elem => elem.charAt(0).toUpperCase() + elem.slice(1)).join('')
+    return (('#' + hashtag).length > 140 || hashtag.length == 0) ? false : '#' + hashtag
+}
+
