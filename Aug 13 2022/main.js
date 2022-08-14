1a658e04bf44ec38572dbb21b0eb15c1318a3701
Samudra Perera <samudrapup@gmail.com>
2022-08-14 00:03:38 -0400

    6 Kyu multiplication table

--- Aug 13 2022/main.js
+++ Aug 13 2022/main.js
@@ -19,9 +19,43 @@ Sum of a Sequence [Hard-Core Version]
 const sequenceSum = (begin, end, step) => {
     let counter = begin;
     let sum = 0
-    while(counter < end) {
+    while(counter <= end) {
         sum += counter;
         counter += step
     }
     return sum
   };
+
+  /* Your task, is to create NxN multiplication table, of size provided in parameter.
+
+for example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+*/
+
+multiplicationTable = function(size) {
+    // Create Empty Table
+    let table = [];
+    //For Loop to create the arrays within table
+    for(let i = 0; i < size; i++) {
+        //Create an empty array within the array of table
+        table[i] = [];
+        //For loop where the iterator i is the multiple in the multiplication table
+        for(let j = 0; j < size; j++) {
+            //For the case of the first element within array j 
+            if(j == 0) {
+                table[i][j] = i + 1
+            } else {
+                // Every element after the first element is equal to the previous plus the iterator + 1
+                table[i][j] = table[i][j-1] + (i + 1)
+            }
+        }
+    }
+    return table
+  }
+
+  multiplicationTable(3)
+  
