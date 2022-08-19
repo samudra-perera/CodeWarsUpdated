a688190e66b11737ae429daa919b099b4f3ce7e5
Samudra Perera <samudrapup@gmail.com>
2022-08-18 20:32:10 -0400

    6kyu Pyramid Maker

--- Aug 18 2022/main.js
+++ Aug 18 2022/main.js
@@ -27,3 +27,77 @@ function DNAStrand(dna){
         }
     }).join('')
   }
+
+  /* In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+At the end of the first year there will be: 
+1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+At the end of the 2nd year there will be: 
+1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+At the end of the 3rd year there will be:
+1141 + 1141 * 0.02 + 50 => 1213
+
+It will need 3 entire years.
+More generally given parameters:
+
+p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+Examples:
+nb_year(1500, 5, 100, 5000) -> 15
+nb_year(1500000, 2.5, 10000, 2000000) -> 10
+Note:
+Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
+*/
+
+function nbYear(p0, percent, aug, p) {
+    let counter = 0;
+    percent = percent/100
+    while (p0 < p) {
+        p0 = Math.floor(p0 + (p0 * percent) + aug)
+        counter++
+    }
+    return counter
+}
+
+/* 
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+Note: the subarrays should be filled with 1s
+*/
+
+function pyramid(n) {
+    let arr = [];
+    for(let i = 0; i < n; i++) {
+        arr[i] = Array(i+1).fill(1)
+    }
+    return arr
+  }
+
+  /*
+Given an array of 3 non-negative integers a, b and c, determine if they form a pythagorean triple.
+
+A pythagorean triple is formed when:
+
+c2 = a2 + b2
+where c is the largest value of a, b, c.
+
+For example: a = 3, b = 4, c = 5 forms a pythagorean triple, because 52 = 32 + 42
+
+Return Values
+1 if a, b and c form a pythagorean triple
+0 if a, b and c do not form a pythagorean triple
+For Python: return True or False
+For JavaScript: return true or false
+*/
+
