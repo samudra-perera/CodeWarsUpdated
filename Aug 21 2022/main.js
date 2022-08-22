e24c660e6f2a9bec71c985006420cf22454ad719
Samudra Perera <samudrapup@gmail.com>
2022-08-21 22:55:21 -0400

    6 kyu what century is it?

--- Aug 21 2022/main.js
+++ Aug 21 2022/main.js
@@ -10,4 +10,53 @@ function lovefunc(flower1, flower2){
 
   // Theory behind this is => 2 evens will make an even and 2 odds will make an even so as long as the sum is odd it is true
 
-  
+  /*
+  I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+*/
+
+function generateShape(integer){
+    let string = '';
+    let newStr = []
+    for(let i = 0; i < integer; i++) {
+        string +='+'
+    }
+    for(let j = 0; j < integer; j++) {
+        if(j == integer - 1) {
+            newStr[j] = string
+            break
+        }
+        newStr[j] = string + '\n'
+    }
+    return newStr.join('')
+}
+
+/*
+Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation.
+
+Examples
+"1999" --> "20th"
+"2011" --> "21st"
+"2154" --> "22nd"
+"2259" --> "23rd"
+"1124" --> "12th"
+"2000" --> "20th"
+*/
+
+function whatCentury(year) {
+    if(year.charAt(1) == '0' && year.charAt(2) == '0' && year.charAt(3) == '0') {
+        return year.charAt(0) + year.charAt(1) + 'th'
+    }
+    
+    let suffix = {
+        '0': 'th','1': 'st', '2': 'nd', '3': 'rd', '4': 'th', '5': 'th', '6': 'th', '7': 'th', '8': 'th', '9': 'th',
+    }
+    let century = (Number(year.charAt(0) + year.charAt(1)) + 1).toString()
+    return (Number(century) < 20) ? century + 'th' : century + suffix[century.charAt(1)]
+}
