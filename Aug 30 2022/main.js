9d766aa287d944bbc4cd3e346e4bc2e346a8dbbd
Samudra Perera <samudrapup@gmail.com>
2022-08-30 03:06:19 -0400

    6 kyu Are they the same

---
+++ Aug 30 2022/main.js
@@ -0,0 +1,59 @@
+/* Define a method/function that removes from a given array of integers all the values contained in a second array.
+
+Examples (input -> output):
+* [1, 1, 2, 3, 1, 2, 3, 4], [1, 3] -> [2, 2, 4]
+* [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+* [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+Enjoy it!! */
+
+Array.prototype.remove_ = function(integer_list, values_list){
+    return integer_list.filter(elem => !values_list.includes(elem))
+  }
+
+/* Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+Note for C
+The two arrays have the same size (> 0) given as parameter in function comp.
+*/
+
+function comp(array1, array2){
+    if(!array1 || !array2) return false;
+    let sqaureArr1 = array1.sort((a,b) => a-b).map(elem => (elem**2).toString()).join('')
+    let sortArr2 = array2.sort((a,b) => a-b).map(elem => elem.toString()).join('')
+
+    return sqaureArr1 == sortArr2 ? true : false
+  }
+
+let comp = (array1, array2) => {
+    if(!array1 || !array2) return false;
+    array1.sort((a,b) => a-b)
+    array2.sort((a,b) => a-b)
+    let counter = array1.map((e,i) => (e**2 == array2[i]) ? e = 1 : e = 0)
+    return counter.reduce((a,c) => a+c, 0) == array1.length
+}
+
+comp([121, 144, 19, 161, 19, 144, 19, 11], [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19])
