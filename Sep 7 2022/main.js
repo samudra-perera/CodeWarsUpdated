7bfd0a23be3e562b0c4209fd8a6a85a48a0a5dd3
Samudra Perera <samudrapup@gmail.com>
2022-09-07 18:21:47 -0400

    5 Kyu firstNonRepeatingLetter

--- Sep 7 2022/main.js
+++ Sep 7 2022/main.js
@@ -132,3 +132,36 @@ function findMissingLetter(array) {
         }
     }
 }
+
+/* Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+*/
+
+function firstNonRepeatingLetter(s) {
+    if(s.length == 0) return '' // an empty string would return an empty string
+    let obj = {}
+    //First for loop to create a set of and count the number of isntance of the letters
+    for(let i = 0; i  < s.length; i++) {
+        let char = s.toLowerCase().charAt(i)
+        if(obj[char]) {
+            obj[char]++
+        } else {
+            obj[char] = 1
+        }
+    }
+
+    for(let i = 0; i < s.length; i++) { //For loop to check the first character that has a value of 1 in the object
+        let char1 = s.toLowerCase().charAt(i)
+        if(obj[char1] == 1) {
+            return s.charAt(i)
+        }
+    }
+
+    return '' //If any character repeats more than once return an empty string
+  }
+
