5313d31b2d878ce05c8ff0d8b445c73ecbacb8fb
Samudra Perera <samudrapup@gmail.com>
2022-09-07 17:48:19 -0400

    6 kyu Find the missing letter

--- Sep 7 2022/main.js
+++ Sep 7 2022/main.js
@@ -102,3 +102,33 @@ function inArray(array1,array2){
     return result 
   }
 
+/* Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+["a","b","c","d","f"] -> "e"
+["O","Q","R","S"] -> "P"
+(Use the English alphabet with 26 letters!)
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+
+*/
+
+function findMissingLetter(array) {
+  let alpha = 'abcdefghijklmnopqrstuvwxyz'; // Alphabet String 
+  let substring =  alpha.slice(alpha.indexOf(array[0].toLowerCase())).split('') // Creating the substring to be searched
+  for(let i = 0; i < array.length; i++) {
+        if(array[i].toLowerCase() == substring[i]) { //checks the array to the substing index
+            continue //if true check the next index
+        } else { //if false return the substring index and check if original input was lower or uppercase
+            return (array[0].toUpperCase() == array[0]) ? substring[i].toUpperCase() : substring[i]
+        }
+    }
+}
