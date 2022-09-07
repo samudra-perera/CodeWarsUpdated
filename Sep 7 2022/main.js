2fda9671e6371ac51a260ca28789952ddf47fe3e
Samudra Perera <samudrapup@gmail.com>
2022-09-07 15:44:35 -0400

    7kyu factorial and 7Kyu counting Valleys

---
+++ Sep 7 2022/main.js
@@ -0,0 +1,55 @@
+/* You need count how many valleys you will pass.
+
+Start is always from zero level.
+
+Every time you go down below 0 level counts as an entry of a valley, and as you go up to 0 level from valley counts as an exit of a valley.
+
+One passed valley is equal one entry and one exit of a valley.
+
+s='FUFFDDFDUDFUFUF'
+U=UP
+F=FORWARD
+D=DOWN
+To represent string above
+
+(level 1)  __
+(level 0)_/  \         _(exit we are again on level 0)
+(entry-1)     \_     _/
+(level-2)       \/\_/
+So here we passed one valley
+*/
+
+function countingValleys(s) {
+    let key = {
+        'F': 0, 'U': 1, 'D': -1
+    }
+    let valleyCount = 0 // Keeps track of the number of valleys
+    let valley = 0; // Keeps track of the current level
+    for(let i = 0; i < s.length; i++) {
+        let char = s.charAt(i) // The current character
+        valley+=key[char]
+        if(valley == 0 && (valley - key[char] < 0)) { //only need to check if we exited a valley 
+            valleyCount++
+        }
+    }
+    return valleyCount
+  }
+
+/* In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
+
+More details about factorial can be found here. */
+
+function factorial(n) {
+  if (n < 0 || n > 12) {
+    throw new RangeError
+  }
+  let total = 1;
+  for (let i = 1; i <= n; i++) {
+    total*=i
+  }
+  return total
+}
+
+factorial(5)
